3273430773b0aeff1d88a2d89e1c90959fcfc1d3
Mohamed Amine Souid <aminesouid20@gmail.com>
2025-05-31 00:58:04 +0200

    corrige code

--- app/src/main/java/com/example/food/MainActivity.kt
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -5,8 +5,5 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d("DEBUG_UI", ">>> MainActivity launched")
-    }
+
 }
